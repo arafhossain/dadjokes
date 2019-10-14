665bc0ba29a7095fa566673c6f3a7b13588fd66e
Araf Hossain <arafh13@gmail.com>
2019-10-14 12:48:07 -0400

    Created JokeList component

---
+++ src/JokeList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class JokeList extends Component {
+  render(){
+    return (
+      <div>
+        <h1> List of Jokes</h1>
+      </div>
+    )
+  }
+}
+
+export default JokeList;
