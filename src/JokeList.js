b9febd6a3a01e3522e67f6ecf40ffb2d1cbba3f5
Araf Hossain <arafh13@gmail.com>
2019-10-14 13:17:13 -0400

    Retrieved intial jokes via axios and setState

--- src/JokeList.js
+++ src/JokeList.js
@@ -1,6 +1,24 @@
 import React, { Component } from 'react'
-
+import axios from 'axios';
 class JokeList extends Component {
+  static defaultProps = {
+    numJokesToGet : 10
+  };
+  constructor(props){
+    super(props);
+    this.state = {jokes: []}
+  }
+  async componentDidMount(){
+    let newJokeArray = [];
+    for(let i = 0; i < this.props.numJokesToGet; i++){
+      let newJoke = await axios.get('https://icanhazdadjoke.com/', {headers: {Accept: 'application/json'}});
+      newJokeArray.push(newJoke.data.joke);
+    }
+    this.setState({
+      jokes: newJokeArray
+    })
+  }
+
   render(){
     return (
       <div>
