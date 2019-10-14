0b89550f4c5eb0569e754b13c5750f66603ef436
Araf Hossain <arafh13@gmail.com>
2019-10-14 16:49:58 -0400

    Added loading screen and isLoading property

--- src/JokeList.js
+++ src/JokeList.js
@@ -10,8 +10,10 @@ class JokeList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      jokes: JSON.parse(window.localStorage.getItem("jokes") || "[]")
+      jokes: JSON.parse(window.localStorage.getItem("jokes") || "[]"),
+      isLoading: false
     };
+    this.handleClick = this.handleClick.bind(this);
   }
   componentDidMount() {
     if (this.state.jokes.length === 0) {
@@ -27,20 +29,39 @@ class JokeList extends Component {
       });
       newJokeArray.push({ joke: newJoke.data.joke, votes: 0, id: uuid() });
     }
-    this.setState({
-      jokes: newJokeArray
-    });
-    window.localStorage.setItem("jokes", JSON.stringify(newJokeArray));
+    this.setState(
+      curState => ({
+        isLoading: false,
+        jokes: [...curState.jokes, ...newJokeArray]
+      }),
+      () =>
+        window.localStorage.setItem("jokes", JSON.stringify(this.state.jokes))
+    );
   }
 
   handleVote(id, change) {
-    this.setState(curState => ({
-      jokes: curState.jokes.map(joke =>
-        joke.id === id ? { ...joke, votes: joke.votes + change } : joke
-      )
-    }));
+    this.setState(
+      curState => ({
+        jokes: curState.jokes.map(joke =>
+          joke.id === id ? { ...joke, votes: joke.votes + change } : joke
+        )
+      }),
+      () =>
+        window.localStorage.setItem("jokes", JSON.stringify(this.state.jokes))
+    );
+  }
+  handleClick() {
+    this.setState({ isLoading: true }, this.getJokes);
   }
   render() {
+    if (this.state.isLoading) {
+      return (
+        <div className="JokeList-loading">
+          <i className="far fa-8x fa-laugh fa-spin" />
+          <h1 className="JokeList-title">Loading</h1>
+        </div>
+      );
+    }
     return (
       <div className="JokeList">
         <div className="JokeList-sidebar">
@@ -51,7 +72,9 @@ class JokeList extends Component {
             src="https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg"
             alt="laugh emoji"
           />
-          <button className="Jokelist-getJoke">Get more Jokes</button>
+          <button className="Jokelist-getJoke" onClick={this.handleClick}>
+            Get more Jokes
+          </button>
         </div>
         <div className="JokeList-jokes">
           {this.state.jokes.map(jokeInfo => (
