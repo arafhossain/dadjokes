df30482d41fdcf7a292911048f7e55f0adf1507c
Araf Hossain <arafh13@gmail.com>
2019-10-14 14:16:43 -0400

    Added sidebar and styling

--- src/JokeList.js
+++ src/JokeList.js
@@ -1,35 +1,44 @@
-import React, { Component } from 'react'
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
+import './JokeList.css'
 class JokeList extends Component {
   static defaultProps = {
-    numJokesToGet : 10
+    numJokesToGet: 10
   };
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state = {jokes: []}
+    this.state = { jokes: [] };
   }
-  async componentDidMount(){
+  async componentDidMount() {
     let newJokeArray = [];
-    for(let i = 0; i < this.props.numJokesToGet; i++){
-      let newJoke = await axios.get('https://icanhazdadjoke.com/', {headers: {Accept: 'application/json'}});
+    for (let i = 0; i < this.props.numJokesToGet; i++) {
+      let newJoke = await axios.get("https://icanhazdadjoke.com/", {
+        headers: { Accept: "application/json" }
+      });
       newJokeArray.push(newJoke.data.joke);
     }
     this.setState({
       jokes: newJokeArray
-    })
+    });
   }
 
-  render(){
+  render() {
     return (
       <div className="JokeList">
-        <h1> List of Jokes</h1>
+        <div className="JokeList-sidebar">
+          <h1 className="JokeList-title">
+            <span>Dad</span> Jokes
+          </h1>
+          <img src="https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg" alt="laugh emoji"/>
+          <button className="Jokelist-getJoke">Get more Jokes</button>
+        </div>
         <div className="JokeList-jokes">
           {this.state.jokes.map(joke => (
             <div>{joke}</div>
           ))}
         </div>
       </div>
-    )
+    );
   }
 }
 
