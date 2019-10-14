4abf309590646a35ffa5bd54b74124007f9a3c00
Araf Hossain <arafh13@gmail.com>
2019-10-14 15:27:35 -0400

    Created joke component and styling

--- src/JokeList.js
+++ src/JokeList.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import axios from "axios";
-import './JokeList.css'
+import "./JokeList.css";
+import Joke from "./Joke";
+import uuid from "uuid/v4";
 class JokeList extends Component {
   static defaultProps = {
     numJokesToGet: 10
@@ -15,13 +17,19 @@ class JokeList extends Component {
       let newJoke = await axios.get("https://icanhazdadjoke.com/", {
         headers: { Accept: "application/json" }
       });
-      newJokeArray.push(newJoke.data.joke);
+      newJokeArray.push({ joke: newJoke.data.joke, votes: 0, id: uuid() });
     }
     this.setState({
       jokes: newJokeArray
     });
   }
-
+  handleVote(id, change) {
+    this.setState(curState => ({
+      jokes: curState.jokes.map(joke =>
+        joke.id === id ? { ...joke, votes: joke.votes + change } : joke
+      )
+    }));
+  }
   render() {
     return (
       <div className="JokeList">
@@ -29,12 +37,22 @@ class JokeList extends Component {
           <h1 className="JokeList-title">
             <span>Dad</span> Jokes
           </h1>
-          <img src="https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg" alt="laugh emoji"/>
+          <img
+            src="https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg"
+            alt="laugh emoji"
+          />
           <button className="Jokelist-getJoke">Get more Jokes</button>
         </div>
         <div className="JokeList-jokes">
-          {this.state.jokes.map(joke => (
-            <div>{joke}</div>
+          {this.state.jokes.map(jokeInfo => (
+            <Joke
+              votes={jokeInfo.votes}
+              text={jokeInfo.joke}
+              key={jokeInfo.id}
+              id={jokeInfo.id}
+              upVote={() => this.handleVote(jokeInfo.id, 1)}
+              downVote={() => this.handleVote(jokeInfo.id, -1)}
+            />
           ))}
         </div>
       </div>
