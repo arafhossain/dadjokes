e9068980908335a6522dc6384be7f8f97c992d9c
Araf Hossain <arafh13@gmail.com>
2019-10-17 11:14:13 -0400

    Updates

---
+++ precache-manifest.090311e0db204920f528c18aa9e0436d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aff980bd2f30d484d3fbdcfbe794c0f0",
+    "url": "/dadjokes/index.html"
+  },
+  {
+    "revision": "e35f957cb2ad1b1d1dab",
+    "url": "/dadjokes/static/css/main.1dbf4d79.chunk.css"
+  },
+  {
+    "revision": "7feef72dcd3447d563cb",
+    "url": "/dadjokes/static/js/2.6f13770f.chunk.js"
+  },
+  {
+    "revision": "e35f957cb2ad1b1d1dab",
+    "url": "/dadjokes/static/js/main.767ed1ea.chunk.js"
+  },
+  {
+    "revision": "7e7447b536aa0d445081",
+    "url": "/dadjokes/static/js/runtime-main.0d8410f5.js"
+  }
+]);
